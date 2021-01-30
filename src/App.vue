e1c970ddba033edaada41c944b0d75648131b36c
Franzbroetchen <franziska.probst@xintec.de>
2021-01-30 12:54:06 +0100

    Project start

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue' // 1.
+
+export default { // 2.
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  /* .. Styling .. */
+}
+</style>
